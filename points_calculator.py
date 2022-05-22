e5ae888d0a5b60837e51cd495ba717cfd27f435e
2894673 <setschmann@gmail.com>
2022-05-22 21:39:57 +0200

    feat: working prototype

--- points_calculator.py
+++ points_calculator.py
@@ -1,18 +1,9 @@
-from ast import parse
-from email import parser
-import pandas as pd
 from datetime import datetime, date
 import argparse
+import pandas as pd
 
 
-
-#import file as df and clean
-df=pd.read_csv("donations.csv", sep = ";")
-df["Annual amount"] = df["Annual amount"].str.replace(",", ".").apply(float)
-df["points"] = 0.0
-
-#functions
-
+# define functions
 def calculate_age(i_birtday) -> int:
     """this function takes a birthday and calculates the current age
     of a person
@@ -28,39 +19,70 @@ def calculate_age(i_birtday) -> int:
     return today.year - birthday.year - ((today.month, 
                                       today.day) < (birthday.month, 
                                                     birthday.day))
-    
-df['Age'] = df['Date of birth'].apply(calculate_age)
+
 
 def calculate_points(i_df):
-    """calculates the points for a df according to a pre defined point scheme
+    """calculates the Points for a df according to a pre defined point scheme
 
     Args:
         i_df (pandas.core.frame.DataFrame): a pandas dataframe
     """    
     for index, row in i_df.iterrows():
         if row["Annual amount"] >= 300 and row["Age"] >= 50:
-            df.loc[index, "points"] += 3
+            i_df.loc[index, "Points"] += 3
         if row["Annual amount"] >= 300 and row["Age"] >= 30:
-            df.loc[index, "points"] += 1.5
+            i_df.loc[index, "Points"] += 1.5
         if row["Annual amount"] >= 150 and row["Age"] >= 50:
-            df.loc[index, "points"] += 2
+            i_df.loc[index, "Points"] += 2
         if row["Annual amount"] >= 150 and row["Age"] >= 30:
-            df.loc[index, "points"] += 1
+            i_df.loc[index, "Points"] += 1
         if row["Annual amount"] >= 120 and row["Age"] >= 50:
-            df.loc[index, "points"] += 1
+            i_df.loc[index, "Points"] += 1
         if row["Annual amount"] >= 120 and row["Age"] >= 30:
-            df.loc[index, "points"] += 0.5
+            i_df.loc[index, "Points"] += 0.5
 
-### main
+def prepare_df(i_df):
+    """wrapper function: prepares the df for further use: changes type from 
+    the "Annual amount" column from string to float, adds an empty "Points" column
+    and adds and calculates an "Age" column.
+    Args:
+        i_df (pandas.core.frame.DataFrame): a pandas df that will be edited in place
+    """    
+    # change type from str to float
+    i_df["Annual amount"] = i_df["Annual amount"].str.replace(",", ".").apply(float)
+    # add Points column
+    i_df["Points"] = 0.0
+    # add age column
+    i_df['Age'] = i_df['Date of birth'].apply(calculate_age)
 
-""" parser = argparse.ArgumentParser(description="Points calculator for fundraising")
-parser.add_argument("--test", required = False, default = True, help="run the tests")
-parser.add_argument()
-parser.add_argument("--fundraiser", required = False, default = "", help="specify one single fundraiser to get the points from")
 
-argument = parser.parse_args()
-status = False """
+def main():
+    # get arguments from cl
+    parser = argparse.ArgumentParser(description="Points calculator for fundraising")
+    parser.add_argument("flag", type=str, help='specify directory or run tests')
+    parser.add_argument("--fundraiser", required = False, type = int, help="specify one single fundraiser to get the Points from")
+    argument = parser.parse_args()
+    
+    if argument.flag == "test":
+        print("run test")
+    
+    elif argument.fundraiser:
+        df=pd.read_csv(argument.flag, sep = ";")
+        subset = df.loc[df["Fundraiser ID"] == argument.fundraiser]
+        if subset.empty:
+             raise Exception("Fundraiser ID doesn't exist")
+        prepare_df(df)
+        calculate_points(df)
+        pd.set_option("display.max_rows", None, "display.max_columns", None) #to show all entries
+        print(df.sort_values(by=["Points"], ascending=False))
+        
+    else:
+        df=pd.read_csv(argument.flag, sep = ";")
+        prepare_df(df)
+        calculate_points(df)
+        pd.set_option("display.max_rows", None, "display.max_columns", None) #to show all entries
+        print(df.sort_values(by=["Points"], ascending=False))
+
 
 if __name__ == "__main__":
-    if argument.test:
-        
+    main()
