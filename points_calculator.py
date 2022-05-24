9060b40c9b30eeb5b35d6a6b9361656e800a6ba2
2894673 <setschmann@gmail.com>
2022-05-24 14:11:00 +0200

    feat: added function to sum up points

--- points_calculator.py
+++ points_calculator.py
@@ -2,6 +2,7 @@ from datetime import datetime, date
 import argparse
 import subprocess
 import pandas as pd
+#import numpy as np
 pd.options.mode.chained_assignment = None #disable warnging for chained pandas df
 
 
@@ -59,6 +60,25 @@ def calculate_points(i_df):
             i_df.loc[index, "Points"] += 0.5
 
 
+def sum_points(i_df):
+    """summarizes the dataframe to view the total points per Fundraiser ID
+
+    Args:
+        i_df (pandas.core.frame.DataFrame): pandas dataframe (column names have to be very
+        speficic for this to work)
+
+    Returns:
+        pandas.core.frame.DataFrame: A pandas df with two rows ("Fundraiser ID"
+        and "Sum of points")
+    """
+    df_sum = pd.DataFrame(columns = ["Fundraiser ID", "Sum of points"])
+    for i, item in enumerate(i_df["Fundraiser ID"].unique().tolist()):
+        sum_points = i_df.loc[:,"Points"].where(i_df["Fundraiser ID"] == item).sum()
+        to_append = [str(item), sum_points]
+        df_sum.loc[i] = to_append
+    return df_sum
+
+
 def main():
     """pulling it all together"""
     # get arguments from cl
@@ -82,7 +102,8 @@ def main():
         prepare_df(subset)
         calculate_points(subset)
         pd.set_option("display.max_rows", None, "display.max_columns", None) #to show all entries
-        print(subset.sort_values(by=["Points"], ascending=False))
+        df_sum_points = sum_points(subset)
+        print(df_sum_points.sort_values(by=["Sum of points"], ascending=False))
 
     # show all results, order by points (ascending)
     else:
@@ -90,7 +111,8 @@ def main():
         prepare_df(df)
         calculate_points(df)
         pd.set_option("display.max_rows", None, "display.max_columns", None) #to show all entries
-        print(df.sort_values(by=["Points"], ascending=False))
+        df_sum_points = sum_points(df)
+        print(df_sum_points.sort_values(by=["Sum of points"], ascending=False))
 
 
 if __name__ == "__main__":
